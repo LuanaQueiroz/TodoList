f1cbf600a4eb6c8dd5663848e19fa265fdaf4fad
Luana Queiroz <102760991+LuanaQueiroz@users.noreply.github.com>
2022-08-02 11:05:46 -0300

    botões active e done
    alternar a cor conforme clicado no botão.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 //alternar janelas
 
 const changeWindow = () =>{
-
+    
+    let bttActivie = document.querySelector('#bttActivie')
+    let bttDone = document.querySelector('#bttDone')
     let contentActivie = document.querySelector('#contentActivie')
     let contentDone1 = document.querySelector('#contentDone1')
 
     if(contentActivie.classList.contains('active')){
         contentActivie.classList.remove('active')
         contentDone1.classList.add('active')
+        bttDone.style.color = '#708DF8'
+        bttActivie.style.color = '#B8B8B8'
 
     }else{
         contentActivie.classList.add('active')
         contentDone1.classList.remove('active')
+        bttDone.style.color = '#B8B8B8'
+        bttActivie.style.color = '#708DF8'
     }
 
-    
-
 }
+
